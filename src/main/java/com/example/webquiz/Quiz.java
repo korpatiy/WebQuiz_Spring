060ae6f1b8121853487c895ae73e3fa9070bdae5
Korpatiy <tretiakovslava@gmail.com>
2020-11-27 22:19:24 +0500

    create new quiz, get quiz by id, return lists of quizzes without answer field, solve the quiz

--- src/main/java/com/example/webquiz/Quiz.java
+++ src/main/java/com/example/webquiz/Quiz.java
@@ -1,41 +1,53 @@
 package com.example.webquiz;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Quiz {
 
+    private int id;
     private String title;
     private String text;
     private List<String> options = new ArrayList<>();
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private int answer;
 
-    public Quiz(String title, String text, List<String> options) {
+    public Quiz(int id, String title, String text, List<String> options, int answer) {
+        this.id = id;
         this.title = title;
         this.text = text;
         this.options = options;
+        this.answer = answer;
+    }
+
+    public Quiz() {
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getText() {
         return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 
     public List<String> getOptions() {
         return options;
     }
 
-    public void setOptions(List<String> options) {
-        this.options = options;
+    public int getAnswer() {
+        return answer;
     }
 }
--- src/main/java/com/example/webquiz/QuizController.java
+++ src/main/java/com/example/webquiz/QuizController.java
@@ -1,5 +1,7 @@
 package com.example.webquiz;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,20 +10,47 @@ import java.util.List;
 @RestController
 public class QuizController {
 
+    private List<Quiz> quizzes = new ArrayList<>();
+    private List<String> options = new ArrayList<>();
+
     public QuizController() {
     }
 
-    @RequestMapping(value = "/api/quiz", method = RequestMethod.POST)
-    public Answer getAnswer(@RequestParam ("answer") int answer) {
-        boolean x = false;
-        if(answer == 3)
-            x = true;
-        return new Answer(x);
+    @RequestMapping(value = "/api/quizzes/{id}/solve", method = RequestMethod.POST)
+    public Answer getAnswer(@PathVariable int id, @RequestParam("answer") int answer) {
+        for (int i = 0; i < quizzes.size(); i++) {
+            if (quizzes.get(i).getId() == id && quizzes.get(i).getAnswer() == answer)
+                return new Answer(true);
+        }
+        return new Answer(false);
     }
 
-    @GetMapping(path = "/api/quiz")
-    public Quiz getQuiz() {
-        List<String> options = new ArrayList<>(List.of("Robot","Tea leaf","Cup of coffee","Bug"));
-        return new Quiz("The Java Logo","What is depicted on the Java logo?", options);
+    @GetMapping(path = "/api/quizzes/{id}")
+    public Object getQuiz(@PathVariable int id) {
+        for (int i = 0; i < quizzes.size(); i++) {
+            if (quizzes.get(i).getId() == id)
+                return quizzes.get(i);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(value = "/api/quizzes", consumes = "application/json")
+    public Quiz createQuiz(@RequestBody Quiz quiz) {
+        quiz.setId((int) (Math.random() * 10));
+        quizzes.add(quiz);
+        return quiz;
+    }
+
+    @RequestMapping(value = "/api/quizzes", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Quiz> getQuizzes() {
+       /* options = new ArrayList<>(List.of("Robot", "Tea leaf", "Cup of coffee", "Bug"));
+        quizzes.add(new Quiz(1, "The Java Logo", "What is depicted on the Java logo?", options, 2));
+        options = List.of("Everything goes right", "42", "2+2=4", "11011100");
+        quizzes.add(new Quiz((int) (Math.random() * 10), "The Ultimate Question",
+                "What is the answer to the Ultimate Question of Life, the Universe and Everything?", options, 1));*/
+        if (quizzes.isEmpty())
+            return new ArrayList<>();
+        return quizzes;
     }
 }
